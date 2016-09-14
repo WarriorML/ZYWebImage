5a78571df8a5cea8a84c37943fa17c7cf87692a6
warrior <wumenglong@aliyun.com>
2016-09-14 10:01:48 +0800

    Initial Commit

---
+++ ZYWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYWebImage
+//
+//  Created by MengLong Wu on 16/9/14.
+//  Copyright © 2016年 MengLong Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYWebImage
+//
+//  Created by MengLong Wu on 16/9/14.
+//  Copyright © 2016年 MengLong Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
